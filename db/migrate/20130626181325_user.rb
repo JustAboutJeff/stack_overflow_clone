ae0e741ff005a2157d50c4339c77b82e22897007
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-26 13:25:05 -0500

    made initial migration files for user, tag, question, comment, answer, and vote

---
+++ db/migrate/20130626181325_user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string  :real_name
+      t.string  :email
+      t.string  :user_name
+      t.string  :password_digest
+      t.text    :about
+      t.integer :age
+      t.string  :website
+      t.string  :location
+
+      t.timestamps
+    end
+  end
+
+end
---
+++ db/migrate/20130626181418_vote.rb
@@ -0,0 +1,11 @@
+class Vote < ActiveRecord::Migration
+  def change
+    create_table :votes do |t|
+      t.integer :value
+      t.integer :user_id
+      t.integer :voteable_id
+      t.string  :voteable_type
+      t.timestamps
+    end
+  end
+end
