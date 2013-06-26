3926a74ca2ee3bb0b24d36ab606ef474d44a91d3
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-26 14:52:48 -0500

    adds sessions to users controller create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,13 @@ class UsersController < ApplicationController
 
   def create
     user = User.create(params[:user])
-    redirect_to user_url(user)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      @errors = user.errors
+      redirect_to new_user_path
+    end
   end
 
   def edit
@@ -24,12 +30,12 @@ class UsersController < ApplicationController
   def update
     user = User.find(params[:id])
     user.update_attributes(params[:user])
-    redirect_to user_url(user)
+    redirect_to user_path(user)
   end
 
   def destroy
     User.destroy(params[:id])
-    redirect_to users_url
+    redirect_to users_path
   end
 
 end
