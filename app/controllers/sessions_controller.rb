6d44386adb88f3c29f4a7c63bd13ec9a56ab863f
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-26 14:48:59 -0500

    shells out sessions controller with actions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  def new
+    # Renders form to allow user login
+  end
+
+  def create
+    if @user = User.authenticate(params[:user][:user_name], params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else 
+      @errors = @user.errors
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+
+end
