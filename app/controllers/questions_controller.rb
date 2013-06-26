6fb14f8d58a833ae4845546971261b0a3020032a
Jordan Nemrow <nemrow@Jordans-MacBook-Pro.local>
2013-06-26 15:42:49 -0500

    questions controller functioning

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,46 @@
+class QuestionsController < ApplicationController
+	
+	def index
+		@questions = Question.all
+	end
+
+	def new
+		@question = Question.new
+	end
+
+	def create
+		question = Question.new(params[:question])
+		# current_user.questions << question
+		current_user = User.find(1)
+		current_user.questions << question
+		if question.save
+			redirect_to questions_path
+		else
+			@errors = "Could not create Question"
+			erb new
+		end
+	end
+
+	def edit
+		@question = Question.find(params[:id])
+	end
+
+	def update
+		question = Question.find(params[:id])
+		# current_user.questions << question
+		current_user = User.find(1)
+		question.update_attributes(params[:question])
+		if question.save
+			redirect_to questions_path
+		else
+			redirect_to questions_path
+		end
+	end
+
+	def destroy
+		question = Question.find(params[:id])
+		question.destroy
+		redirect_to questions_path
+	end
+
+end
