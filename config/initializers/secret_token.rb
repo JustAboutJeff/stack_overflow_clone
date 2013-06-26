c73160d9abb60ee713a7af1a4f151aa940d33fa9
JEFF <justaboutjeff@gmail.com>
2013-06-26 10:20:40 -0500

    Initial commit, rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StackOverflowClone::Application.config.secret_token = '8f3808f2516e7a63c68bb0166fb271eca10a26406d565413cd381251c288628377613a032a75fd5684d0062cce95deecde504bbfe81fbf3e1aaf2bb234adf082'
