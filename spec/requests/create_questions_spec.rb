32c56101a88502ec00c81d2abe1483aafbb62eff
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-28 17:06:50 -0500

    Integration tests revised and passing

--- spec/requests/create_questions_spec.rb
+++ spec/requests/create_questions_spec.rb
@@ -1,8 +1,6 @@
 require 'spec_helper'
 
 describe "User can create questions" do
-  let(:user) { FactoryGirl.create(:user) }
-  let(:question) { FactoryGirl.create(:question) }
 
   it "allow user to login" do
     login
@@ -10,18 +8,18 @@ describe "User can create questions" do
     expect(uri.path).to eq(root_path)
   end
 
-  # it "allow user to signup" do
-  #   visit new_user_path
-  #   fill_in "user_user_name", :with => "niles"
-  #   fill_in "user_email", :with => "niles@niles.com"
-  #   fill_in "user_password", :with => "123456"
-  #   fill_in "user_password_confirmation", :with => "123456"
-  #   click_button "Sign up"
-  #   expect(page).to have_content "niles"
-  # end
+  it "allow user to signup" do
+    visit new_user_path
+    fill_in "user_user_name", :with => "niles"
+    fill_in "user_email", :with => "niles@niles.com"
+    fill_in "user_password", :with => "123456"
+    fill_in "user_password_confirmation", :with => "123456"
+    click_button "Sign up"
+    expect(page).to have_content "niles"
+  end
 
   it "allows user profile edit" do
-    login
+    user = login
     visit user_path(user)
     click_link "Edit"
     fill_in "user_user_name", :with => "Franz"
@@ -31,14 +29,12 @@ describe "User can create questions" do
     expect(page).to have_content "Franz@Franzy.com"
   end
 
-  # it "allows a user to post a question" do
-  #   pending
-  #   # TODO: MAKE THIS TEST PASS
-  #   # visit root_path
-  #   # click_link "Add New Question"
-  #   # fill_in "question_title", :with => question.title
-  #   # fill_in "question_content", :with => question.content
-  #   # click_button "Create Question"
-  #   # expect(page).to have_content question.title
-  # end
+  it "allows a user to post a question" do
+    login
+    click_link "Add New Question"
+    fill_in "question_title", :with => "Strange Facts from Beyond"
+    fill_in "question_content", :with => "Science ipsum. Entropy thus"
+    click_button "Create Question"
+    expect(page).to have_content "Science"
+  end
 end
