5f1f002239f07f3e516d8c26db5d2a5d019a5e31
Jordan Nemrow <nemrow@Jordans-MacBook-Pro.local>
2013-06-27 21:22:26 -0500

    made voting logic universal for comments, votes, and questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,11 +5,6 @@ class Question < ActiveRecord::Base
   has_many :answers
   has_many :comments, :as => :commentable
   has_many :votes, :as => :voteable
-  
   has_many :questions_tags
   has_many :tags, :through => :questions_tags
-
-  def vote_score
- 		self.votes.sum('value')
- 	end
 end
